ba04fbc119880e7e9609b3604bab3a08bff0d2e2
Mabrural <mabruralmutaqi@gmail.com>
2024-09-02 11:46:43 +0700

    hidden button bdr

--- manage-delivery-order.php
+++ manage-delivery-order.php
@@ -99,12 +99,12 @@ if (!isset($_SESSION["login"])) {
                           <a href="edit-delivery.php?id_do=<?= $data['id_do']?>" class="btn btn-warning btn-sm"><i class="fa fa-pen fa-sm"></i> Edit</a>
                           <a href="#" class="btn btn-danger btn-sm" onclick="return confirmRemove(<?= $data['id_do']?>);"><i class="fa fa-trash fa-sm"></i> Remove</a>
                           <!-- <a href="tambah-bdr.php?id_do=<?= $data['id_do']?>" class="btn btn-primary btn-sm"><i class="fa fa-file-alt fa-sm"></i> BDR</a> -->
-                          <?php if (!$bdrExists): ?>
+                          <!-- <?php if (!$bdrExists): ?> -->
                               <!-- Jika tidak ada data di tabel bdr, tampilkan tombol BDR -->
-                              <a href="tambah-bdr.php?id_do=<?= $id_do ?>" class="btn btn-primary btn-sm"><i class="fa fa-file-alt fa-sm"></i> BDR</a>
+                              <!-- <a href="tambah-bdr.php?id_do=<?= $id_do ?>" class="btn btn-primary btn-sm"><i class="fa fa-file-alt fa-sm"></i> BDR</a> -->
                           <?php else: ?>
                               <!-- Jika ada data di tabel bdr, tampilkan tombol View BDR -->
-                              <a href="view-bdr.php?id_bdr=<?= $data['id_bdr'] ?>" class="btn btn-secondary btn-sm"><i class="fa fa-eye fa-sm"></i> View BDR</a>
+                              <!-- <a href="view-bdr.php?id_bdr=<?= $data['id_bdr'] ?>" class="btn btn-secondary btn-sm"><i class="fa fa-eye fa-sm"></i> View BDR</a> -->
                           <?php endif; ?>
                         </td>
                       </tr>

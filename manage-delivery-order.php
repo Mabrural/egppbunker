cbaec0fad35f5b459b6d645a8b32897fac6b725d
mabrural <mabruralmutaqi@gmail.com>
2024-09-12 17:48:27 +0700

    tambah-bdr.php sudah berhasil insert data namun belum ada tampilannya di frontend

--- manage-delivery-order.php
+++ manage-delivery-order.php
@@ -98,7 +98,7 @@ if (!isset($_SESSION["login"])) {
                           <a href="#" class="btn btn-info btn-sm" onclick="printContent(<?= $data['id_do']?>);"><i class="fa fa-print fa-sm"></i> Print</a>
                           <a href="edit-delivery.php?id_do=<?= $data['id_do']?>" class="btn btn-warning btn-sm"><i class="fa fa-pen fa-sm"></i> Edit</a>
                           <a href="#" class="btn btn-danger btn-sm" onclick="return confirmRemove(<?= $data['id_do']?>);"><i class="fa fa-trash fa-sm"></i> Remove</a>
-                          <!-- <a href="tambah-bdr.php?id_do=<?= $data['id_do']?>" class="btn btn-primary btn-sm"><i class="fa fa-file-alt fa-sm"></i> BDR</a> -->
+                          <a href="tambah-bdr.php?id_do=<?= $data['id_do']?>" class="btn btn-primary btn-sm"><i class="fa fa-file-alt fa-sm"></i> BDR</a>
                           <!-- <?php if (!$bdrExists): ?> -->
                               <!-- Jika tidak ada data di tabel bdr, tampilkan tombol BDR -->
                               <!-- <a href="tambah-bdr.php?id_do=<?= $id_do ?>" class="btn btn-primary btn-sm"><i class="fa fa-file-alt fa-sm"></i> BDR</a> -->

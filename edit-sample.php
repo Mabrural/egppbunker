d753f6f3c760f169d76f5ff1d2672b62416b0714
Mabrural <mabruralmutaqi@gmail.com>
2024-10-08 10:16:07 +0700

    edit-sample.php sudah berhasil dibuat

---
+++ edit-sample.php
@@ -0,0 +1,201 @@
+<?php 
+session_name("EGPPBUNKER_SESSION");
+session_start();
+
+if (!isset($_SESSION["login"])) {
+  header("Location: login.php");
+  exit;
+}
+
+
+    include "koneksi.php";
+    $id_user = $_SESSION["id_user"];
+    $nama = $_SESSION["nama"];
+    $level = $_SESSION['is_admin'];
+
+    $id_sample = mysqli_real_escape_string($koneksi, $_GET['id_sample']);
+
+    $sample = query("SELECT * FROM sample_receipt JOIN delivery_order ON delivery_order.id_do=sample_receipt.do_id WHERE id_sample=$id_sample")[0];
+
+
+    // cek apakah tombol submit sudah ditekan atau belum
+    if (isset($_POST['editSample']) ) {
+	
+        // cek apakah data berhasil update atau tidak
+        if(editSample($_POST) > 0 ) {
+            echo '<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css"></script>';
+            echo '<link rel="stylesheet" href="./sweetalert2.min.css"></script>';
+            echo '<script src="./sweetalert2.min.js"></script>';
+            echo "<script>
+            setTimeout(function () { 
+                swal.fire({
+                    
+                    title               : 'Success',
+                    text                : 'Sample Receipt successfully updated!',
+                    icon                : 'success',
+                    timer               : 2000,
+                    showConfirmButton   : false
+                });  
+            },10);   setTimeout(function () {
+                window.location.href = 'manage-delivery-order.php'; //will redirect to your blog page (an ex: blog.html)
+            }, 2000); //will call the function after 2 secs
+            </script>"; 
+            exit;
+
+        } else{
+            echo '<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css"></script>';
+            echo '<link rel="stylesheet" href="./sweetalert2.min.css"></script>';
+            echo '<script src="./sweetalert2.min.js"></script>';
+            echo "<script>
+            setTimeout(function () { 
+                swal.fire({
+                    
+                    title               : 'Failed',
+                    text                : 'Failed to updated Sample Receipt!',
+                    icon                : 'error',
+                    timer               : 2000,
+                    showConfirmButton   : false
+                });  
+            },10);   setTimeout(function () {
+                window.location.href = 'manage-delivery-order.php'; //will redirect to your blog page (an ex: blog.html)
+            }, 2000); //will call the function after 2 secs
+            </script>";
+            exit;
+
+        }
+    
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    include "layouts/head-css.php";
+?>
+
+<style>
+    span#x{
+        color: red;
+    }
+
+    .remove-button {
+    display: inline-flex;
+    align-items: center;
+    background-color: transparent;
+    border: none;
+    color: red;
+    cursor: pointer;
+    font-size: 12px;
+}
+
+.remove-buttons-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 10px;
+}
+
+.remove-button {
+    display: flex;
+    align-items: center;
+    background-color: transparent;
+    border: none;
+    color: red;
+    cursor: pointer;
+    font-size: 12px;
+    text-align: left;
+}
+</style>
+
+<body>
+
+<?php
+    include "layouts/header.php";
+?>
+
+<?php
+    include "layouts/sidebar.php";
+?>
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Edit Sample Receipt</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+          <li class="breadcrumb-item active">Edit Sample Receipt</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-8">
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">DO Number : <?= $sample['do_number']?></h5>
+        
+                    <!-- Horizontal Form -->
+                    <form action="" method="post">
+                        <input type="hidden" name="id_sample" value="<?= $id_sample?>">
+                        <input type="hidden" name="do_id" value="<?= $sample['do_id']?>">
+                        
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <div class="mb-2">
+                                    <label for="truck_or_vessel" class="form-label">Truck / Vessel <span id="x">*</span></label>
+                                    <input type="text" name="truck_or_vessel" value="<?= $sample['truck_or_vessel']?>" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <div class="mb-2">
+                                    <label for="cargo" class="form-label">Cargo / <i>Muatan </i><span id="x">*</span></label>
+                                    <input type="text" name="cargo" value="<?= $sample['cargo']?>" class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <div class="mb-2">
+                                    <label for="port" class="form-label">Port / <i>Pelabuhan</i> <span id="x">*</span></label>
+                                    <input type="text" name="port" value="<?= $sample['port']?>" class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="text-left">
+                        <button type="submit" class="btn btn-primary btn-sm" name="editSample"><i class="fa fa-file-invoice fa-sm"></i> Update</button>
+                        <a href="manage-delivery-order.php" class="btn btn-danger btn-sm"><i class="fa fa-times fa-sm"></i> Cancel</a>
+                        </div>
+                    </form><!-- End Horizontal Form -->
+        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+   
+
+  </main><!-- End #main -->
+
+<?php
+    include "layouts/footer.php";
+?>
+
+<?php
+    include "layouts/script-js.php";
+?>
+  
+
+</body>
+
+</html>
